7e8920003b85aa0003736cfcc4fdaa776045bc15
deta-aditya <deta.aditya@tokopedia.com>
2022-03-21 20:33:38 +0700

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import logo from './logo.svg'
+
+function App() {
+  return (
+    <>
+      <header>
+        <h1>Pokeme</h1>
+      </header>
+      <main>Hello, world!</main>
+    </>
+  )
+}
+
+export default App
