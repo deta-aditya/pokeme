163bd1fb7be4248f41f54244366fab32057d60a6
deta-aditya <deta.aditya@tokopedia.com>
2022-03-22 15:55:59 +0700

    make pokemon list infinite

--- src/Home/Home.tsx
+++ src/Home/Home.tsx
@@ -1,45 +1,24 @@
-import axios from "axios"
-import { useEffect, useState } from "react"
-import { Link } from "react-router-dom"
 import { NavBar } from "../NavBar"
-
-type PokemonListItem = {
-  name: string
-  url: string
-}
+import { PokemonsInfiniteList } from "../PokemonsInfiniteList";
 
 function Home() {
-  const [pokemons, setPokemons] = useState<PokemonListItem[]>([])
-
-  useEffect(() => {
-    getPokemonsList()
-      .then(setPokemons)
-      .catch(err => console.error(err))
-  }, [])
-
   return (
-    <>
-      <header>
+    <div style={{
+      display: 'flex',
+      flexDirection: 'column',
+      height: '100vh',
+    }}>
+      <header style={{
+        paddingBottom: '16px',
+        borderBottom: '1px solid #eee',
+        flexGrow: 0,
+      }}>
         <h1>Pokeme</h1>
         <NavBar />
       </header>
-      <main>
-        <hr />
-        <div>
-          {pokemons.map(({ name }, idx) => (
-            <div key={idx}>
-              <Link to={`/pokemons/${name}`}>{name}</Link>
-            </div>
-          ))}
-        </div>
-      </main>
-    </>
+      <PokemonsInfiniteList />
+    </div>
   )
-}
-
-function getPokemonsList(): Promise<PokemonListItem[]> {
-  return axios.get('https://pokeapi.co/api/v2/pokemon/?limit=20')
-    .then(response => response.data.results.map((result: any) => result as PokemonListItem))
 }
 
 export { Home }
