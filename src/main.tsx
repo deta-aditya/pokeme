f3dc5bafbf6bff80e4973c79c95e15cde26d5a48
deta-aditya <deta.aditya@tokopedia.com>
2022-03-27 21:44:33 +0700

    configure and add icons for pwa

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,12 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
 import App from './App'
+import { Workbox } from 'workbox-window'
+
+if ('serviceWorker' in navigator) {
+  const wb = new Workbox('/sw.js')
+  wb.register()
+}
 
 ReactDOM.render(
   <React.StrictMode>
--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,48 @@ import { VitePWA } from 'vite-plugin-pwa'
 export default defineConfig({
   plugins: [
     react(),
-    VitePWA(),
+    VitePWA({
+      base: "/",
+      includeAssets: [
+        'favicon.svg', 
+        'favicon.ico', 
+        'robots.txt', 
+        'apple-touch-icon.png',
+      ],
+      manifest: {
+        "name": "Pokeme",
+        "short_name": "Pokeme",
+        "icons": [
+          {
+            "src": "/android-chrome-144x144.png",
+            "sizes": "144x144",
+            "type": "image/png"
+          },
+          {
+            "src": "/android-chrome-192x192.png",
+            "sizes": "192x192",
+            "type": "image/png"
+          },
+          {
+            "src": "/android-chrome-512x512.png",
+            "sizes": "512x512",
+            "type": "image/png"
+          },
+          {
+            "src": "/android-chrome-512x512.png",
+            "sizes": "512x512",
+            "type": "image/png",
+            "purpose": "any maskable"
+          }
+        ],
+        "start_url": "/",
+        "theme_color": "#ffffff",
+        "background_color": "#ffffff",
+        "display": "standalone"
+      },
+      devOptions: {
+        enabled: true
+      },
+    }),
   ]
 })
